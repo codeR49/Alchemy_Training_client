ed0db9a6d4dea36fd9d654db89ca7aefdaafdc39
ArnabC49 <46952316+codeR49@users.noreply.github.com>
2022-06-14 13:03:23 +0530

    signin redirect

--- src/Page/Signin/Signin.js
+++ src/Page/Signin/Signin.js
@@ -1,70 +1,111 @@
-import React, { useState } from 'react'
-import {Helmet} from 'react-helmet';
+import React, { useState } from "react";
+import { Helmet } from "react-helmet";
+import { Routes, useNavigate } from "react-router-dom";
 import "./Signin.css";
-import axios from 'axios';
+import axios from "axios";
+import jwt_decode from "jwt-decode";
+import { useAuth } from "../../Component/Utils/Auth";
 
 function Signin() {
-    // const [isPasswordShow, setIsPasswordShow] = useState(false);
-    let [username, setUsername] = useState('');
-    let [password, setPassword] = useState('');
-    
-    // const toggleIsPasswordShowValue = () => {
-    //     setIsPasswordShow(!isPasswordShow);
-    // };
-    let onchangeusernamehandler = (event) => {
-        setUsername(event.target.value);
-        console.log(event.target.value)
-    }
-    let onchangepasswordhandler = (event) => {
-        setPassword(event.target.value);
-        console.log(event.target.value)
-    }
-const submithandler = (e)=>{
-e.preventDefault()
-const Data = {
-    username:username,
-    password:password
-}
+  // const [isPasswordShow, setIsPasswordShow] = useState(false);
+  const auth = useAuth();
+  let [username, setUsername] = useState("");
+  let [password, setPassword] = useState("");
+  let [error, setErrorMessage] = useState("");
+  let message;
+  // const toggleIsPasswordShowValue = () => {
+  //     setIsPasswordShow(!isPasswordShow);
+  // };
+  let navigate = useNavigate();
+  let onchangeusernamehandler = (event) => {
+    setUsername(event.target.value);
+    console.log(event.target.value);
+  };
+  let onchangepasswordhandler = (event) => {
+    setPassword(event.target.value);
+    console.log(event.target.value);
+  };
+  const submithandler = (e) => {
+    e.preventDefault();
+    const Data = {
+      username: username,
+      password: password,
+    };
 
-axios.post("http://localhost:3000/users/login",Data)
-.then((res)=>{
-console.log(res)
-})
-}
+    axios
+      .post("http://localhost:8080/users/login", Data)
+      .then((res) => {
+        let { token } = res.data;
 
-    return (
-       <>
-        <Helmet>
-                <style>{'body { background-color: #F5F8FB }'}</style>
-            </Helmet>
-        <div class="container">
-            <div class="wrapper">
-                <div class="title"><span>Sign in</span></div>
-                <form onSubmit={submithandler}>
-                    <div class="row">
-                        <i class="fas fa-user"></i>
+        localStorage.setItem("token", token);
 
-                        <input type="text" placeholder="User Name" onChange={onchangeusernamehandler} id='username' required />
-                    </div>
-                    <div class="row">
+        if (res.status === 200) {
+          let roles = jwt_decode(localStorage.getItem("token")).roles;
+          let name = jwt_decode(localStorage.getItem("token")).name;
+          localStorage.setItem("roles", roles);
+          localStorage.setItem("name", name);
+          auth.login();
+          message = (
+            <Routes>
+              {roles === "Training Executive"
+                ? navigate("/i", { replace: true })
+                : "/"}
+            </Routes>
+          );
 
-                        <i class="fas fa-lock"></i>   
+          return message;
+        }
+      })
+      .catch((error) => {
+        console.log("error occured");
+        console.log(error.data);
+        setErrorMessage("Enter valid Username and Password");
+      });
+  };
 
-                        <input type= 'password' placeholder="Password" id='password' onChange={onchangepasswordhandler} required />
-                        
-                    </div>
-                   
+  return (
+    <>
+      <Helmet>
+        <style>{"body { background-color: #F5F8FB }"}</style>
+      </Helmet>
+      <div class="container">
+        <div class="wrapper">
+          <div class="title">
+            <span>Sign in</span>
+          </div>
+          <form onSubmit={submithandler}>
+            <div class="row">
+              <i class="fas fa-user"></i>
 
-                    <div class="row button">
-                        <input type="submit" value="Login" />
-                    </div>
-
-                </form>
+              <input
+                type="text"
+                placeholder="User Name"
+                onChange={onchangeusernamehandler}
+                id="username"
+                required
+              />
             </div>
+            <div class="row">
+              <i class="fas fa-lock"></i>
+
+              <input
+                type="password"
+                placeholder="Password"
+                id="password"
+                onChange={onchangepasswordhandler}
+                required
+              />
+            </div>
+
+            <div class="row button">
+              <button type="submit">Login</button>
+            </div>
+          </form>
+          <p>{error}</p>
         </div>
-       
-       </>
-    )
+      </div>
+    </>
+  );
 }
 
-export default Signin
+export default Signin;
